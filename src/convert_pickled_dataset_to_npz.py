10e966a379fb31399e0a5f521a77458f836454bd
ashish <katnoria.ashish@gmail.com>
2019-08-31 14:10:59 +0800

    Added script to convert list of pickle files into npz, autoencoder notebook

--- src/convert_pickled_dataset_to_npz.py
+++ src/convert_pickled_dataset_to_npz.py
@@ -16,15 +16,28 @@ def load_pickle(fname):
     array = pickle.load(open(fname, 'rb'))
     return array
 
-fnames = glob('dataset/*.pkl')
-num_files_to_process = 10
 
-process_fnames = fnames[:num_files_to_process]
-available_cpus = psutil.cpu_count()
-ray.init(num_cpus=available_cpus)
-start = time()
-dataset = ray.get([load_pickle.remote(fname) for fname in process_fnames])
-dataset = [subitem for item in dataset for subitem in item]
-np.savez('test.npz', dataset)
-print('Took {} seconds'.format(time() - start))
+def convert_states(input_fnames, out_dirname='processed/'):
+    if not os.path.exists(out_dirname):
+        os.makedirs(out_dirname)
 
+    available_cpus = psutil.cpu_count()
+    ray.init(num_cpus=available_cpus)
+    start = time()
+    dataset = ray.get([load_pickle.remote(fname) for fname in input_fnames])
+    dataset = np.asarray([subitem[0] for item in dataset for subitem in item])
+    out_fname = '{}/{}rollouts.npz'.format(out_dirname, len(fnames))
+    np.savez(out_fname, dataset)
+    print('saved {}'.format(out_fname))
+    print('Took {} seconds'.format(time() - start))
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--rollouts", default=10, type=int, help="Number of rollouts to process into a single npz file")
+    args = parser.parse_args()
+
+    fnames = glob('dataset/*.pkl')
+    process_fnames = fnames[:args.rollouts]
+    convert_states(process_fnames)
