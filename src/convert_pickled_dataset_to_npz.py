b301b5da9741246400b3d2b5106c6541cb312769
katnoria <katnoria.ashish@gmail.com>
2019-08-31 12:03:50 +0800

    Added the script to convert pickled data into npz

---
+++ src/convert_pickled_dataset_to_npz.py
@@ -0,0 +1,29 @@
+import argparse
+import pickle
+import logging
+import os
+import uuid
+from datetime import datetime
+from glob import glob
+from time import time
+import numpy as np
+
+import psutil
+import ray
+
+@ray.remote
+def load_pickle(fname):
+    array = pickle.load(open(fname, 'rb'))
+    return array
+
+fnames = glob('dataset/*.pkl')
+num_files_to_process = 10
+
+process_fnames = fnames[:num_files_to_process]
+available_cpus = psutil.cpu_count()
+ray.init(num_cpus=available_cpus)
+start = time()
+dataset = ray.get([load_pickle.remote(fname) for fname in process_fnames])
+np.savez('test.npz', dataset)
+print('Took {} seconds'.format(time() - start))
+
