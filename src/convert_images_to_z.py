78d4e94d4a40268da8492b93792f9cd5cc13f915
ashish <katnoria.ashish@gmail.com>
2019-10-07 22:54:53 +0800

    Added code to convert pkl (s,a,s') to npz format

--- src/convert_images_to_z.py
+++ src/convert_images_to_z.py
@@ -7,6 +7,55 @@ from datetime import datetime
 from glob import glob
 from time import time
 import numpy as np
+import tensorflow as tf
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+
+# Load images from the dataset and run it through
+# VAE model to generate compressed Z representation
+
+def load_npz_data(fname):
+    """
+    Loads npz data into array
+    """
+    with np.load(fname) as data:
+        np_data = data['arr_0']
+    return np_data
+
+def load_preprocess_image(fname):
+    image = tf.io.read_file(fname)
+    import pdb; pdb.set_trace()
+    image = tf.image.decode_jpeg(image, channels=3)
+    image = tf.image.resize(image, [64, 64])
+    image /= 255.0
+    return image    
+
+def load_resized_image(items):
+    obs_img, action, next_obs_img = items    
+    obs_img /= 255.
+    next_obs_img /= 255.
+
+    return obs_img, action, next_obs_img
+
+def load_data(dirname, max_items=None):
+    fnames = glob("{}/*.pkl".format(dirname))
+    size = len(fnames) if not max_items else max_items
+    fnames = np.random.choice(fnames, size)
+    ds = tf.data.Dataset.from_tensor_slices(fnames)    
+    dataset = ds.map(load_resized_image, num_parallel_calls=AUTOTUNE)
+    items = dataset.take(1)
+    print(items)
+
+    
+
+def process():
+    data = load_npz_data('processed_with_actions/50rollouts.npz')
+    # data = load_data('dataset/', 10)
+    print(data[0][0].shape)
+    ds = tf.data.Dataset.from_tensor_slices(data)    
+    dataset = ds.map(load_preprocess_image, num_parallel_calls=AUTOTUNE)
 
 
-# Load
+
+if __name__ == "__main__":
+    process()
--- src/convert_pickled_dataset_to_npz.py
+++ src/convert_pickled_dataset_to_npz.py
@@ -7,14 +7,30 @@ from datetime import datetime
 from glob import glob
 from time import time
 import numpy as np
+from PIL import Image
 
 import psutil
 import ray
 
+SIZE = 64, 64
+
+@ray.remote
+def resize_image(img_array):    
+    im = Image.fromarray(img_array)
+    im.thumbnail(SIZE)
+    return np.array(im)
+
 @ray.remote
 def load_pickle(fname):
-    array = pickle.load(open(fname, 'rb'))
-    return array
+    arraylist = pickle.load(open(fname, 'rb'))
+    output = []
+    for obs, action, next_obs in arraylist:
+        output.append((
+            ray.get(resize_image.remote(obs)),
+            action,
+            ray.get(resize_image.remote(next_obs))
+        ))
+    return output
 
 
 def convert_states(input_fnames, out_dirname='processed/'):
@@ -25,7 +41,21 @@ def convert_states(input_fnames, out_dirname='processed/'):
     ray.init(num_cpus=available_cpus)
     start = time()
     dataset = ray.get([load_pickle.remote(fname) for fname in input_fnames])
-    dataset = np.asarray([subitem[0] for item in dataset for subitem in item])
+    dataset = np.asarray([subitem[0] for item in dataset for subitem in item])    
+    out_fname = '{}/{}rollouts.npz'.format(out_dirname, len(input_fnames))
+    np.savez(out_fname, dataset)
+    print('saved {}'.format(out_fname))
+    print('Took {} seconds'.format(time() - start))
+
+def resize_and_compress_buffer(input_fnames, out_dirname='processed_with_actions/'):
+    if not os.path.exists(out_dirname):
+        os.makedirs(out_dirname)
+
+    available_cpus = psutil.cpu_count()
+    ray.init(num_cpus=available_cpus)
+    start = time()
+    dataset = ray.get([load_pickle.remote(fname) for fname in input_fnames])
+    dataset = np.asarray([subitem for item in dataset for subitem in item])
     out_fname = '{}/{}rollouts.npz'.format(out_dirname, len(input_fnames))
     np.savez(out_fname, dataset)
     print('saved {}'.format(out_fname))
@@ -40,4 +70,5 @@ if __name__ == "__main__":
 
     fnames = glob('dataset/*.pkl')
     process_fnames = fnames[:args.rollouts]
-    convert_states(process_fnames)
+    # convert_states(process_fnames)
+    resize_and_compress_buffer(process_fnames)
